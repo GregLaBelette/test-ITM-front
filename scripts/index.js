8b832c33acf06b565aef0a6f6ea9ec22b3b6eb19
Gregoire Vallette <gregoire.vallette@gmail.com>
2022-01-06 10:38:27 +0100

    resuming work update OK without chartkick integration

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,76 @@
 const host = 'http://localhost:3000'
+const euros = new Intl.NumberFormat("fr-FR", { style: "currency", currency: "EUR", currencyDisplay: "symbol" })
 
-window.onload = () => {
-  updateFields();
+// View Updates
+
+const updateCount = (data) => {
+  document.getElementById('count').innerHTML = `${new Intl.NumberFormat().format(parseInt(data.count))} orders`
 }
 
-const updateFields = () => {
-  fetch(host)
-  .then(res => res.json)
-  .then((data) => { console.log(data) })
+const updateCountries = (data) => {
+  const dropdown = document.getElementById('countries');
+  dropdown.innerHTML = '<option selected value="">All countries selected</option>';
+  data.countries.forEach((country) => {
+    const list = `<option value="${country}">${country}</option>`
+    dropdown.insertAdjacentHTML('beforeend', list)
+  });
+}
+
+const updateSummary = (data) => {
+  document.getElementById('revenue').innerHTML=`${euros.format(data.revenue)}`
+  document.getElementById('average').innerHTML=`${euros.format(data.average_per_order)}`
+  document.getElementById('customers').innerHTML=`${data.customers}`
+}
+
+const updateChart = (chart) => {
+  document.getElementById('chart').innerHTML = chart
+  console.log(chart)
+}
+
+// API calls
+
+const fetchCountries = () => {
+  fetch(host,{ headers: { 'Accept': 'application/json' } } )
+  .then(res => res.json())
+  .then((data) => {
+    updateCountries(data);
+    addCountriesListener(data);
+  })
+}
+
+const fetchData = (country) => {
+  fetch(`${host}?country=${country}`,{ headers: { 'Accept': 'application/json' } } )
+  .then(res => res.json())
+  .then((data) => {
+    updateCount(data);
+    updateSummary(data);
+  })
+}
+
+const fetchChart = (country) => {
+  fetch(`${host}/chart?country=${country}`,{ headers: { 'Accept': 'text/plain' } } )
+  .then(res => res.text())
+  .then((chart) => {
+    updateChart(chart);
+  })
+}
+
+// Listeners & triggers
+
+const update = (country) => {
+  fetchData(country);
+  fetchChart(country);
+}
+
+window.onload = () => {
+  fetchCountries();
+  update('');
+}
+
+function addCountriesListener(data) {
+  document.getElementById('countries').childNodes.forEach((elt) => {
+    elt.addEventListener('click', (e) => {
+      update(elt.value )
+    })
+  })
 }
